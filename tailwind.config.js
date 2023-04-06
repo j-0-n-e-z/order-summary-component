87b56bbe276b370c7a0c0106611d01331b0f6d1f
j-0-n-e-z <j000neeez@gmail.com>
2023-04-06 18:33:26 +0500

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+	content: ['./dist/**/*.{html,js}'],
+	theme: {
+		extend: {
+			colors: {
+				paleBlue: 'hsl(225, 100%, 94%)',
+				brightBlue: 'hsl(245, 75%, 52%)',
+				veryPaleBlue: 'hsl(225, 100%, 98%)',
+				desaturatedBlue: 'hsl(224, 23%, 55%)',
+				darkBlue: 'hsl(223, 47%, 23%)'
+			},
+			backgroundImage: {
+				desktop: "url('./assets/pattern-background-desktop.svg')",
+				mobile: "url('./assets/pattern-background-mobile.svg')"
+			}
+		}
+	},
+	plugins: []
+}
